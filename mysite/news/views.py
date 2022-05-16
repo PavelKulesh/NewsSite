0a5396726172bf4629c834d2ffbd23c15394942d
Oleg <oleg.krivenkov.2002@mail.ru>
2022-05-16 18:18:23 +0300

    initial commit

---
+++ mysite/news/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from news.models import News
+
+def index(request):
+    news = News.objects.all()
+    res= '<h1>List of News</h1>'
+    for item in news:
+        res += f'<div>\n<p>{item.title}<p>\n<p>{item.content}<p>\n<div>\n<hr>\n'
+    return HttpResponse(res)
+
+
+def test(request):
+    return HttpResponse('<h1> Test page <h1>')
