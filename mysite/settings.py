fbf72b039b9560ca69a827865b92d0a5e2d4fe10
Oleg <oleg.krivenkov.2002@mail.ru>
2022-05-29 18:57:39 +0300

    refactor: hide secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*1-f93fn90w00-f@1joe^dboxcim0hk$_(zny#uo@xdy1yl7_4'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- news/forms.py
+++ news/forms.py
@@ -27,11 +27,12 @@ class NewsForm(forms.ModelForm):
     class Meta:
         model = News
         fields = ['title', 'content', 'is_published','photo', 'category']
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
-        }
+        # widgets = {
+        #     'title': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        #     'category': forms.Select(attrs={'class': 'form-control'}),
+        #     # 'photo':forms.FileField()
+        # }
 
 
 
